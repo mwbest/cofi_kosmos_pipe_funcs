203605f9af25378f7bcc281438032fd4b33842aa
RhettButlerP <137355915+RhettButlerP@users.noreply.github.com>
2023-07-14 09:31:01 -0600

    Update darkcombine_and_apo_proc.py

--- CofI_KOSMOS/darkcombine_and_apo_proc.py
+++ CofI_KOSMOS/darkcombine_and_apo_proc.py
@@ -8,8 +8,8 @@ import pandas as pd
 from astropy.table import Table
 from astropy import units as u
 from astropy.io import fits
-__all__ = ['apo_proc', 'darkcombine']
 
+__all__ = ['apo_proc', 'darkcombine']
 def apo_proc(file, bias = None, flat = None, dark = None,
          trim = True, ilum = None, Saxis = 0, Waxis = 1,
          EXPTIME = 'EXPTIME', DATASEC = 'DATASEC',
